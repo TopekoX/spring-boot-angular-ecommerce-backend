908a9df7e034644290e9da7faedda0e437f7de2f
Ucup Topekox <ucup.topekox@gmail.com>
2022-05-03 13:07:59 +0800

    set CrossOrigin

--- src/main/java/com/topekox/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/topekox/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.topekox.ecommerce.dao;
 import com.topekox.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category") // this annotation to custom url
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
